9e1570c7910c792c7d2ab4e5d17fc5c2b935648b
Cleyson Braga de Oliveira <CleysonOliveira@i3967924.local>
2022-10-07 22:55:06 -0300

    breakdown into files to clean it up

---
+++ utils.js
@@ -0,0 +1,75 @@
+//Misc Helpers
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+    return `${x}x${y}`;
+}
+  
+function createName() {
+    const prefix = randomFromArray([
+      "COOL",
+      "SUPER",
+      "SOFT",
+      "BUFF",
+      "DOPE",
+    ]);
+    const animal = randomFromArray([
+      "MOLE",// 0-0
+    ]);
+  
+    return `${prefix} ${animal}`;
+}
+  
+function isSolid(x,y) {
+  
+    const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
+    return (
+      blockedNextSpace ||
+      x >= mapData.maxX ||
+      x < mapData.minX ||
+      y >= mapData.maxY ||
+      y < mapData.minY
+    )
+}
+
+function getRandomSpot() {
+    let x = randomIntFromInterval(mapData.minX, mapData.maxX-1);
+    let y = randomIntFromInterval(mapData.minY, mapData.maxY-1);
+
+    return { x, y };
+}
+
+function getRandomSafeSpot() {
+    //We don't look things up by key here, so just return an x/y
+    return randomFromArray([
+      { x: 1, y: 4 },
+      { x: 2, y: 4 },
+      { x: 1, y: 5 },
+      { x: 2, y: 6 },
+      { x: 2, y: 8 },
+      { x: 2, y: 9 },
+      { x: 4, y: 8 },
+      { x: 5, y: 5 },
+      { x: 5, y: 8 },
+      { x: 5, y: 10 },
+      { x: 5, y: 11 },
+      { x: 11, y: 7 },
+      { x: 12, y: 7 },
+      { x: 13, y: 7 },
+      { x: 13, y: 6 },
+      { x: 13, y: 8 },
+      { x: 7, y: 6 },
+      { x: 7, y: 7 },
+      { x: 7, y: 8 },
+      { x: 8, y: 8 },
+      { x: 10, y: 8 },
+      { x: 8, y: 8 },
+      { x: 11, y: 4 },
+    ]);
+}
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
