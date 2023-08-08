11ff2c6c07c3657fa2e8294557fc371ff883edbf
H.Cley <44215827+HCley@users.noreply.github.com>
2023-08-07 22:09:28 -0300

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,12 +1,12 @@
 (function name() {
   const firebaseConfig = {
-    apiKey: "AIzaSyC8TCAQbzPITyyBL5WcDtRnZe_cUP5qots",
-    authDomain: "jeyndkey.firebaseapp.com",
-    databaseURL: "https://jeyndkey-default-rtdb.firebaseio.com",
-    projectId: "jeyndkey",
-    storageBucket: "jeyndkey.appspot.com",
-    messagingSenderId: "627657547872",
-    appId: "1:627657547872:web:e1b9048102a2d1a07f6305",
+    apiKey: "AIzaSyC8TCAQbzPITy*********_cUP5qots",
+    authDomain: "je*****ey.firebaseapp.com",
+    databaseURL: "https://*********-default-rtdb.firebaseio.com",
+    projectId: "******-******",
+    storageBucket: "*****.appspot.com",
+    messagingSenderId: "627********7872",
+    appId: "1:*********:web:*********************",
   };
 
   // Initialize Firebase
